a71f2f2868edfd09051de62780f32eb594ecee61
frnkly <frank@frnk.ca>
2019-05-25 23:03:23 -0400

    Added Travis-CI file

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,5 +13,5 @@ CONTAINER_ID=$(docker container ls | grep ${COMPOSE_PROJECT_NAME}_api | cut -c 1
 if [[ $CONTAINER_ID == "" ]]; then
     echo "No container running. Use the \"./run\" script to launch it."
 else
-    docker exec --interactive --tty $CONTAINER_ID bash -c "go test ./... -cover"
+    docker exec --interactive --tty $CONTAINER_ID bash -c "go test -cover ./..."
 fi
